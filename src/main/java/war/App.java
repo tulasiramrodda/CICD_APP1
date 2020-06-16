69bc812e64f78b94c267fce65e04a031cfb9cf88
tulasiramrodda <46471865+tulasiramrodda@users.noreply.github.com>
2020-06-16 14:25:08 +0530

    Update App.java

--- src/main/java/war/App.java
+++ src/main/java/war/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!!!! This is CICD APP from scratch" );
+        System.out.println( "Hello World!!!! This is CICD APP from scratch to build jar file" );
     }
 }
